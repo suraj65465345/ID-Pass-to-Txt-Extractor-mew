dbc9fead79850a1d9e0ac019096166da918b6c4f
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-05-06 19:34:57 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+api_id = 28919553
+api_hash = "2ed4532cc8353ab77179cb291ffeb86e"
+bot_token = "6472474798:AAEPR6JMAj5Jw52Og5tHoPPxkgGanGuVIJA"
+auth_users = "1871785273, 7023381717, 7134852010 "
+sudo_users = [int(num) for num in auth_users.split(",")]
+osowner_users = "1871785273, 7023381717, 7134852010"
+owner_users = [int(num) for num in osowner_users.split(",")]
