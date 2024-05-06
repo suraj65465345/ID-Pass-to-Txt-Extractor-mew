b837b853135e89ffdda5a3e0253121aaec15db13
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-05-06 20:00:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 28528007
 api_hash = "38464da16c80310cabc8d13952419cf3"
 bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "6329158981 "
+auth_users = "6329158981"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "6329158981"
 owner_users = [int(num) for num in osowner_users.split(",")]
