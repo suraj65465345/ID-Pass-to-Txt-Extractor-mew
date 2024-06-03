65bccf7741d9dfbf17ce89c783051f386bdd18bf
suraj65465345 <suraj65465345@gmail.com>
2024-06-03 16:46:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28528007
-api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "6329158981"
+api_id = 22223501
+api_hash = "e78c151d670ab33c5f7f731027c5ab26"
+bot_token = "7329591675:AAGaT6f8u-NcGNL4ytq0XTFNkYZL_SS8bd8"
+auth_users = "6235754855"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6329158981"
+osowner_users = "6235754855"
 owner_users = [int(num) for num in osowner_users.split(",")]
